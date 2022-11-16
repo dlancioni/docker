797f9c5acbc50c84d87921e28f5cd923189104bd
David Lancioni <dlancioni@gmail.com>
2022-11-16 16:35:46 -0300

    Docker training

---
+++ Php/html/index.php
@@ -0,0 +1,23 @@
+<?php
+$conn = new mysqli("29238f30fa4d","root","admin","db1");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+$sql = "SELECT 1+1 total";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["total"] ;
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+
+?>
