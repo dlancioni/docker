36586127f98390013b4c5ced28098f6ee01b9c25
David Lancioni <dlancioni@gmail.com>
2022-11-27 15:00:18 -0300

    fn message

--- Mysql/ecommerce/scripts/sp/sp_tb_person.sql
+++ Mysql/ecommerce/scripts/sp/sp_tb_person.sql
@@ -1,14 +1,14 @@
 /*
 delete from tb_person;
 select * from tb_person;
-call sp_tb_person('i', null, 1, 1, 'name 2', '2022-12-31', @sid, @msg);
-call sp_tb_person('u', 1, 1, 1, 'name 1', '2021-01-01', @sid, @msg);
-call sp_tb_person('d', 0, null, null, null, null, @sid, @msg);
-call sp_tb_person('s', 1, null, null, null, null, @sid, @msg);
-call sp_tb_person('s', null, null, null, null, null, @sid, @msg);
+call sp_tb_person('i', null, 1, 1, 'name 2', '2022-12-31', @v_st, @v_msg);
+call sp_tb_person('u', 1, 1, 1, 'name 1', '2021-01-01', @v_st, @v_msg);
+call sp_tb_person('d', 0, null, null, null, null, @v_st, @v_msg);
+call sp_tb_person('s', 1, null, null, null, null, @v_st, @v_msg);
+call sp_tb_person('s', null, null, null, null, null, @v_st, @v_msg);
 
-call sp_tb_person('i', null, 0, 1, 'name 2', '2022-12-31', @sid, @msg);
-select @sid, @msg
+call sp_tb_person('i', null, 0, 1, 'name 2', '2022-12-31', @v_st, @v_msg);
+select @v_st, @v_msg
 
 */
 use ecommerce;
@@ -23,8 +23,8 @@ create procedure sp_tb_person
 	in p_classification_id int,
     in p_name varchar(50),
 	in p_birth date,
-    out sid int,
-    out msg varchar(500)
+    out v_st int,
+    out v_msg varchar(500)
 )
 language sql
 deterministic
@@ -35,26 +35,28 @@ sp: begin
     declare v_count int default 0;
 	declare v_value varchar(500) default '';    
 
-	set sid = 0;
-	set msg = '';
+	set v_st = 0;
+	set v_msg = '';
     set p_action = upper(p_action);
     
 	if p_action not in ('I', 'U', 'D', 'S') then
-        select fn_msg(1, '', 1) into msg;
+		set v_value = '';
+        select fn_msg(1, v_value, 1) into v_msg;
 		leave sp;
     end if;
 
 	if p_action = 'U' or p_action = 'D' then
     
 		if p_id is null or p_id = 0 then
-			select fn_msg(2, fn_label('tb_person.id'), 1) into msg;
+			set v_value = fn_label('tb_person.id');
+			select fn_msg(2, v_value, 1) into v_msg;
 			leave sp;
 		end if;
                 
         select count(id) into v_count from tb_person where id = p_id;        
 		if v_count = 0 then
-			v_value = concat(fn_label('tb_person.id'), ',', p_id, ',', fn_label('tb_person'));
-			select fn_msg(3, v_value, 3) into msg;
+			set v_value = concat(fn_label('tb_person.id'), ',', p_id, ',', fn_label('tb_person'));
+			select fn_msg(3, v_value, 3) into v_msg;
 			leave sp;
 		end if;
 
@@ -64,21 +66,21 @@ sp: begin
     
 		-- mandatory
 		if p_type_id is null or p_type_id = 0 then
-			v_value = fn_label('tb_person.type_id');
-			select fn_msg(2, v_value, 1) into msg;
+			set v_value = fn_label('tb_person.type_id');
+			select fn_msg(2, v_value, 1) into v_msg;
 			leave sp;
 		end if;
 
 		-- field size
 		if length(p_name) > 50 then
-			select fn_get_message(4, 'tb_person', 'name', '', '') into msg;
+			select fn_get_message(4, 'tb_person', 'name', '', '') into v_msg;
 			leave sp;
 		end if;
        
 		-- foreign keys		
 		select id from tb_person_type where id = p_type_id;
         if found_rows() = 0 then
-			select fn_get_message(3, 'tb_person', 'type_id', 'tb_person_type', 'id') into msg;
+			select fn_get_message(3, 'tb_person', 'type_id', 'tb_person_type', 'id') into v_msg;
 			leave sp;
         end if;		
 
@@ -122,7 +124,7 @@ sp: begin
 
     end if;
 
-	set sid = 1;
-	set msg = 'processado com sucesso !!!';
+	set v_st = 1;
+	set v_msg = 'processado com sucesso !!!';
 end
 $$
