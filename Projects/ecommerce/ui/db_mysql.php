6d3c4d10a3f3a3f7c0c4bfa353a7a4ddf8166212
David Lancioni <dlancioni@gmail.com>
2022-12-24 11:04:50 -0300

    Coding

--- Projects/ecommerce/ui/db_mysql.php
+++ Projects/ecommerce/ui/db_mysql.php
@@ -7,6 +7,7 @@ function connect()
     $password = "admin";
     $dbname = "ecommerce";
 
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
@@ -19,19 +20,35 @@ function connect()
 function query($conn, $sql)
 {
     $result = $conn->query($sql);
-    return $result;
+
+    if ($result->num_rows > 0) 
+    {
+        while($row = $result->fetch_assoc()) 
+        {
+            echo "id: " . $row["id"]. " - Ds: " . $row["ds"] . "<br>";
+        }
+    }
+    
 }
 
+function execute($conn, $sql)
+{
+    //$stmt = $conn->prepare($sql);
+    //$stmt->bind_param("is", $id, $output);
+    //$stmt->execute();
+
+    $conn->multi_query( "CALL sp_test(1, @output);select @output as output" );
+    $conn->next_result();
+    $rs=$conn->store_result();
+    echo $rs->fetch_object()->output, "\n";
+    $rs->free();
+
+
+}
 
 $conn = connect();
-$result = query($conn, "select * from tb_person_type");
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Ds: " . $row["ds"] . "<br>";
-    }
-  }
-
+// query($conn, "select * from tb_person_type");
+execute($conn, "CALL sp_test(?, ?)");
 $conn->close();
 
 ?>
